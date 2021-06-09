b3b2d8774008d0d61ed2a67c5432b2c8bab7823c
shivam jadhav <jadhavshivam0228@gmail.com>
2021-06-09 16:16:56 +0530

    fixed box plot dark mode bug and added feature selection python function

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 
 myDataFrame=None
 myFeatures=None
+train_data = None
+CHOICES = None
+categorical = None
+numerical = None
 """
 0 : file path doesnt exist
 1 : file type errors
@@ -22,16 +26,19 @@ def main(file_pth):
     file_pth = file_pth[1:-1]
     data = check_for_errors(file_pth)
     global myDataFrame
+    global myFeatures
+    global categorical
+    global numerical
     myDataFrame=data
     correlationmatrix=myDataFrame.corr().values.tolist()
     correlationmatrixKeys=list(myDataFrame.corr().to_dict().keys())
     formattedCorrelationMatrix=heat_map(correlationmatrixKeys,correlationmatrix)
     columns = list(data.columns)
-    global myFeatures
+
     myFeatures=columns
     column_dtypes = np.array(data.dtypes).astype(str).tolist()
     categorical = [columns[idx] for idx,tp in enumerate(column_dtypes) if tp=='object']
-    numerical =   [columns[idx] for idx,tp in enumerate(column_dtypes) if tp=='int64'] 
+    numerical =   [columns[idx] for idx,tp in enumerate(column_dtypes) if tp=='int64']
     return np.array(data.head(10)).tolist(), columns, numerical, categorical,formattedCorrelationMatrix
 
 
@@ -79,4 +86,31 @@ def GetFeatureValues(featureName):
         info = np.array(list(myDataFrame[featureName].describe()))[mask]
         info[2] = np.median(myDataFrame[featureName])
         return info.tolist()
+
+@eel.expose
+def get_user_choices(choices): #choices = [] ==>> list of all choices numerical union categoricals
+    global CHOICES
+    CHOICES = choices
+    preprocessing_data()
+    print(train_data)
+    return train_data.tolist()
+
+def preprocessing_data():
+    global numerical
+    global categorical
+    global CHOICES
+    data = myDataFrame[CHOICES]
+    new_cats = [cat for cat in CHOICES if cat in categorical]
+    new_nums = [num for num in CHOICES if num in numerical]
+
+    cat_data = myDataFrame[new_cats]
+    num_data = myDataFrame[new_nums]
+
+    num_data = pd.DataFrame(data=StandardScaler().fit_transform(num_data), columns=num_data.columns)
+    cat_data = data.aggregate(LabelEncoder().fit_transform)
+    cat_data = pd.DataFrame(data=StandardScaler().fit_transform(cat_data), columns=cat_data.columns)
+
+    global train_data
+    train_data = pd.concat([num_data, cat_data], axis=1).values
+
 eel.start("main.html",size=(1000,700))
